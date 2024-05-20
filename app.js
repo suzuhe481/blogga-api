ba16b76796487696ccf5dcd24c440daf9e97fd8c
Hector <baseballhector@gmail.com>
2024-05-20 17:28:37 -0400

    Set up mongoDB connection

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Set up a Mongoose connection
+mongoose.set("strictQuery", false);
+
+const dev_db_url = "placeholder";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// Wait for database connect. Logs error if fail to connect.
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 var app = express();
 
 // view engine setup
