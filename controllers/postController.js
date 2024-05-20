e93b7de59abd2ae564300882560edc2bd68d091e
Hector <baseballhector@gmail.com>
2024-05-20 17:19:50 -0400

    Controller functions for post resource

---
+++ controllers/postController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("express-async-handler");
+
+// Post Routes
+// GET - get all posts
+exports.GET_ALL_POSTS = asyncHandler(async (req, res, next) => {
+  return res.send("GET - get all posts");
+});
+
+// // GET - get a single post
+exports.GET_ONE_POST = asyncHandler(async (req, res, next) => {
+  console.log(req);
+  return res.send(`GET - get a single post - ID: ${req.params.postId}`);
+});
+
+// // POST - create a single post
+exports.POST_ONE_POST = asyncHandler(async (req, res, next) => {
+  return res.send(
+    `POST - create a single post - Title: ${req.body.title} - Post: ${req.body.post}`
+  );
+});
+
+// // PUT - update a single post
+exports.PUT_ONE_POST = asyncHandler(async (req, res, next) => {
+  return res.send(`PUT - update a single post - ID: ${req.params.postId}`);
+});
+
+// // DELETE - delete a single post
+exports.DELETE_ONE_POST = asyncHandler(async (req, res, next) => {
+  return res.send(`DELETE - delete a single post - ID: ${req.params.postId}`);
+});
