cf244e2587c82324a40668999ad44e16b31d105e
Hector <baseballhector@gmail.com>
2024-06-09 20:14:49 -0400

    Home page route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 const passport = require("passport");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET - home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
