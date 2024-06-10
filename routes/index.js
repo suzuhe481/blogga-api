a97dcc341792c3fba688d6af6f08cf7d8b44f413
Hector <baseballhector@gmail.com>
2024-06-09 20:19:57 -0400

    Login route that sends user a JWT (token) with passport

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,27 @@ router.get("/register", function (req, res, next) {
   res.render("register", { title: "Register" });
 });
 
+/* POST - Login the user */
+// Uses passport to login and authenticate the user.
+// Returns a JWT object as a response to valid user.
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+  }),
+  // Below only runs after a successful login.
+  function (req, res) {
+    // Creates a tokenObject for the logged in user.
+    const tokenObject = issueJWT(req.user);
+
+    // Sends token to user in JSON.
+    res.status(200).json({
+      success: true,
+      user: req.user,
+      token: tokenObject.token,
+      expiresIn: tokenObject.expires,
+    });
+  }
+);
+
 module.exports = router;
