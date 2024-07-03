fec2351901112a2a2ac3bce275c8a2fde454bb91
Hector <baseballhector@gmail.com>
2024-07-03 17:54:58 -0400

    Session configuration

--- app.js
+++ app.js
@@ -65,9 +65,14 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
+    saveUninitialized: false,
     store: sessionStore,
+    unset: "destroy", // Removes session from database
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+      secure: false,
+      sameSite: false,
+      httpOnly: true,
     },
   })
 );
