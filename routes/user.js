6528823aeff808f23fafac6876091c9b5dabdd3f
Hector <baseballhector@gmail.com>
2024-07-14 15:38:30 -0400

    New route to retrieve user's name

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const user_controller = require("../controllers/userController");
 
 // Routes for resources
 router.get("/", user_controller.GET_ALL_USERS);
+router.get("/name", user_controller.GET_SELF_NAME);
 router.get("/:id", user_controller.GET_ONE_USER);
 router.post("/", user_controller.POST_ONE_USER);
 router.put("/:id", user_controller.PUT_ONE_USER);
