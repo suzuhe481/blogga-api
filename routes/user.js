8f31bb40faf7285540e9faaa1e7c7a3f96329db7
Hector <baseballhector@gmail.com>
2024-10-21 18:11:33 -0400

    New routes for user settings

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,8 @@ const user_controller = require("../controllers/userController");
 // Routes for resources
 router.get("/", user_controller.GET_ALL_USERS);
 router.get("/name", user_controller.GET_SELF_NAME);
+router.get("/settings", user_controller.GET_SETTINGS);
+router.put("/settings", user_controller.PUT_SETTINGS);
 router.get("/:id", user_controller.GET_ONE_USER);
 router.post("/", user_controller.POST_ONE_USER);
 router.put("/:id", user_controller.PUT_ONE_USER);
