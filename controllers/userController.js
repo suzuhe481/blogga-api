ed52a43f35d8d3e3e8161521a15123dc885eeac7
Hector <baseballhector@gmail.com>
2024-10-26 14:27:48 -0400

    Route to create a user with encrypted password

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,14 +20,85 @@ exports.GET_ONE_USER = asyncHandler(async (req, res, next) => {
   return res.send(`GET - get a single user - ID: ${req.params.id}`);
 });
 
-// // POST - create a single user
+// POST - create a single user.
+// Public to all.
 exports.POST_ONE_USER = asyncHandler(async (req, res, next) => {
-  return res.send(
-    `POST - create a single user - First: ${req.body.first_name} Last: ${req.body.last_name}`
-  );
+  // Get date at time of creating account.
+  const account_created_date = new Date().toISOString();
+
+  // Creating a test user object, with form's given email
+  // const user = new User({
+  //   first_name: "First",
+  //   last_name: "Last",
+  //   email: req.body.email,
+  //   account_created_date: date,
+  //   status: "Member",
+  // });
+
+  const userPreferences = new UserPreferences();
+
+  // Creates a user object.
+  const user = new User({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    username: req.body.username,
+    email: req.body.email,
+    account_created_date: account_created_date,
+    status: "Member",
+    verified: false,
+    user_preferences: userPreferences,
+  });
+
+  // console.log(`User is: ${user}`);
+
+  const ErrorMessages = [];
+
+  // Checks if email has already been used.
+  const emailIsTaken = await User.findOne({ email: req.body.email }).exec();
+
+  const usernameIsTaken = await User.findOne({
+    username: req.body.username,
+  }).exec();
+
+  if (emailIsTaken) {
+    ErrorMessages.push("Email is already in use.");
+  }
+
+  if (usernameIsTaken) {
+    ErrorMessages.push("Username is already in use.");
+  }
+
+  if (ErrorMessages.length > 0) {
+    // console.log("errors exist");
+    // console.log(ErrorMessages);
+
+    return res.status(401).json({
+      error: true,
+      message: ErrorMessages,
+    });
+  } else {
+    try {
+      // Creates a hashed password
+      const hashedPassword = await generatePassword(req.body.password);
+
+      // Assigns hashed password to user object.
+      user.password = hashedPassword;
+
+      // Save new user and redirect to home page.
+      await user.save();
+
+      // await transporter.sendMail(mailData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return res.status(200).json({
+    success: true,
+    user: user,
+  });
 });
 
-// // PUT - update a single user
 exports.PUT_ONE_USER = asyncHandler(async (req, res, next) => {
   return res.send(`PUT - update a single user - ID: ${req.params.id}`);
 });
