92c04483438e43a694b7a80d548551c58d426099
Hector <baseballhector@gmail.com>
2024-10-27 21:36:12 -0400

    Origin is the array variable of ORIGIN_URLS

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ if (process.env.DEV_MODE === "false") {
 // app.use(cors()); // Works
 app.use(
   cors({
-    origin: "https://ff520800.blogga-frontend.pages.dev",
+    origin: ORIGIN_URLS,
     credentials: true,
   })
 );
