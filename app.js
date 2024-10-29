cb9ca313c1b6712d1cf3f5bcb1fbcb91625a38d4
Hector <baseballhector@gmail.com>
2024-10-28 22:52:36 -0400

    Commented out proxy in production session

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ else {
       saveUninitialized: false,
       store: sessionStore,
       unset: "destroy", // Removes session from database
-      proxy: true,
+      // proxy: true,
       cookie: {
         maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
         // maxAge: 1000 * 60 * 60, // 1 hour
