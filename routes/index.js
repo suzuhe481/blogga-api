4ca053f2a4524d36b4f7144de1738a39de281064
Hector <baseballhector@gmail.com>
2024-10-31 19:43:36 -0400

    Route to check if a given email is taken

--- routes/index.js
+++ routes/index.js
@@ -120,4 +120,19 @@ router.get("/verify", isUser, hasJWT, async function (req, res, next) {
   });
 });
 
+// Route that checks if a given email is already taken by another user.
+router.get("/check-email/:email", async function (req, res, next) {
+  // RegExp to search case insensitive
+  const emailRegex = new RegExp(`^${req.params.email}$`, "i");
+
+  // Checks if email has already been used.
+  const user = await User.findOne({ email: { $regex: emailRegex } }).exec();
+
+  const emailAvailable = user ? false : true;
+
+  return res.status(200).json({
+    emailAvailable: emailAvailable,
+  });
+});
+
 module.exports = router;
