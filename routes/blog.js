47a84105bb27b401a7c50cbf87cb87e8aaad8335
Hector <baseballhector@gmail.com>
2024-11-22 17:05:49 -0500

    New route to allow a blog owner to toggle a blog's published status

--- routes/blog.js
+++ routes/blog.js
@@ -10,5 +10,6 @@ router.post("/", blog_controller.POST_ONE_BLOG);
 router.put("/:id", blog_controller.PUT_ONE_BLOG);
 router.delete("/:id", blog_controller.DELETE_ONE_BLOG);
 router.get("/draft/:id", blog_controller.GET_ONE_DRAFT);
+router.put("/toggle/:id", blog_controller.TOGGLE_VISIBILITY);
 
 module.exports = router;
